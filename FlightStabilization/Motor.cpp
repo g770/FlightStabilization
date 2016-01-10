93221f2a40bbd6bb96133787f028fe75e9524602
g770 <g770@users.noreply.github.com>
2016-01-10 16:06:12 -0700

    Add comments and validation

--- FlightStabilization/Motor.cpp
+++ FlightStabilization/Motor.cpp
@@ -1,6 +1,8 @@
 
 #include "Motor.h"
 
+// Min and max throttle pulse width in microseconds
+// These are typical values used by ESCs
 const int MIN_THROTTLE = 1000;
 const int MAX_THROTTLE = 2000;
 
@@ -13,11 +15,30 @@ void Motor::init(uint8_t pin, int minThrottle, int maxThrottle)
 
 void Motor::arm()
 {
+	// Arm the motor by sending a throttle off command
 	this->motorControl.writeMicroseconds(MIN_THROTTLE);
 }
 
 void Motor::writeThrottle(int throttleValue)
 {
+	if (!this->throttleValueValid(throttleValue))
+	{
+		return;
+	}
+
+	// First map the throttle value into the correct pulse
+	// width to send to the motor, then write it
 	int microsecs = map(throttleValue, this->minThrottle, this->maxThrottle, MIN_THROTTLE, MAX_THROTTLE);
+
 	this->motorControl.writeMicroseconds(microsecs);
+}
+
+bool Motor::throttleValueValid(int throttleValue)
+{
+	if (throttleValue < this->minThrottle || throttleValue > this->maxThrottle)
+	{
+		return false;
+	}
+
+	return true;
 }
--- FlightStabilization/Motor.h
+++ FlightStabilization/Motor.h
@@ -15,11 +15,22 @@ class Motor
 {
 
  public:
+	 // Initialize the motor class
+	 // pin -- The pin the motor is connected to
+	 // minThrottle, maxThrottle -- The min and max values that may be 
+	 // sent to the writeThrottle command.  These are typically the values 
+	 // read from a receiver.
 	 void init(uint8_t pin, int minThottle, int maxThrottle);
+	 
+	 // Arms the motor
 	 void arm();
+
+	 // Writes a throttle value to the motor
 	 void writeThrottle(int throttleValue);
 
  private:
+	 bool throttleValueValid(int throttleValue);
+
 	Servo motorControl;
 	int minThrottle;
 	int maxThrottle;
